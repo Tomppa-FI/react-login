374ac1fdc529b3dd23a001cd57b9cf0d78b99ccb
Tomppa-FI <tomppafi@protonmail.com>
2020-01-17 15:33:39 +0200

    Slight cleanup to Navbar/App

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import './App.css';
+import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar />
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            mainDisplay: undefined
+        }
+    }
+
+    setMainDisplay = component => {
+        this.setState({
+            mainDisplay: component
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Navbar />
+                    <Switch>
+                        <Route path="/register">
+                            <RegisterForm />
+                        </Route>
+                        <Route path="/login">
+                            <LoginForm />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
 
 export default App;
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,37 +1,20 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import RegisterForm from "./RegisterForm";
-import LoginForm from "./LoginForm";
+import {Link} from "react-router-dom";
 import "./Navbar.css";
 
 class Navbar extends React.Component {
     render() {
         return (
-            <Router>
-                <nav className="Navbar">
-                    <ul className="Navbar-list">
-                        <li className="Navbar-item">
-                            <Link to="/register" className="Navbar-link">Sign Up</Link>
-                        </li>
-                        <li className="Navbar-item">
-                            <Link to="/login" className="Navbar-link">Log In</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/register">
-                        <RegisterForm />
-                    </Route>
-                    <Route path="/login">
-                        <LoginForm />
-                    </Route>
-                </Switch>
-            </Router>
+            <nav className="Navbar">
+                <ul className="Navbar-list">
+                    <li className="Navbar-item">
+                        <Link to="/register" className="Navbar-link">Sign Up</Link>
+                    </li>
+                    <li className="Navbar-item">
+                        <Link to="/login" className="Navbar-link">Log In</Link>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
