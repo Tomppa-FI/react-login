62e3bcdd0650f233325141667f5122e375f9b5ab
Tomppa-FI <tomppafi@protonmail.com>
2020-01-17 15:00:39 +0200

    Added base form styling & structure

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -7,18 +7,19 @@ import {
 } from "react-router-dom";
 import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
+import "./Navbar.css";
 
 class Navbar extends React.Component {
     render() {
         return (
             <Router>
                 <nav className="Navbar">
-                    <ul>
-                        <li>
-                            <Link to="/register">Sign Up</Link>
+                    <ul className="Navbar-list">
+                        <li className="Navbar-item">
+                            <Link to="/register" className="Navbar-link">Sign Up</Link>
                         </li>
-                        <li>
-                            <Link to="/login">Log In</Link>
+                        <li className="Navbar-item">
+                            <Link to="/login" className="Navbar-link">Log In</Link>
                         </li>
                     </ul>
                 </nav>
--- app/src/components/RegisterForm.js
+++ app/src/components/RegisterForm.js
@@ -1,10 +1,66 @@
 import React from "react";
+import "./RegisterForm.css";
 
 class RegisterForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            hash: "",
+            errors: {}, 
+        }
+    }
+    handleSubmit = () => {
+
+    }
+
+    handleChange = event => {
+
+    }
     render() {
         return (
-            <div>
+            <div className="FormContainer">
+                {this.state.errors.status && this.state.errors.status.errorMsg}
+                <form className="RegisterForm" onSubmit={this.handleSubmit}>
+                    <div className="RegisterForm-inputContainer">
+                        <input 
+                            className="RegisterForm-input"
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            onChange={this.handleChange}
+                        />
 
+                        {this.state.errors.username && this.state.errors.username.errorMsg}
+                    </div>
+                    <div className="RegisterForm-inputContainer">
+                        <input 
+                            className="RegisterForm-input"
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={this.handleChange}
+                        />
+                        {this.state.errors.password && this.state.errors.password.errorMsg}
+                    </div>
+                    <div className="RegisterForm-inputContainer">
+                        <input 
+                            className="RegisterForm-input"
+                            type="password"
+                            name="confirmPassword"
+                            placeholder="Repeat Password"
+                            onChange={this.handleChange}
+                        />
+                        {this.state.errors.confirmPassword && this.state.errors.confirmPassword.errorMsg}
+                    </div>
+
+                    <div className="RegisterForm-inputContainer">
+                        <input
+                            className="RegisterForm-submitBtn"
+                            type="submit" 
+                        />
+                    </div>
+                </form>
             </div>
         )
     }
